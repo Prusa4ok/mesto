5fe593a3eb4fa3ec86fc9d1a20a3070c64b9dad2
Ruslan Mokrov <greenbpost@gmail.com>
2020-09-15 10:11:07 +0600

    feat js initial 6 cards

--- script/script.js
+++ script/script.js
@@ -8,6 +8,44 @@ const profileName = page.querySelector('.profile__name');
 const profileDescription = page.querySelector('.profile__description');
 const popupName = page.querySelector('.popup__input_adding_name');
 const popupDescription = page.querySelector('.popup__input_adding_description');
+const tempCard = page.querySelector('#tempCard').content;
+const cardsList = page.querySelector('.cards__list');
+
+const initialCards = [
+	{
+		name: 'Архыз',
+		link: 'images/card-arhyz.jpg'
+	},
+	{
+		name: 'Челябинская область',
+		link: 'images/card-chelyabinsk-obl.jpg'
+	},
+	{
+		name: 'Иваново',
+		link: 'images/card-ivanovo.jpg'
+	},
+	{
+		name: 'Камчатка',
+		link: 'images/card-kamchatka.jpg'
+	},
+	{
+		name: 'Холмогорский район',
+		link: 'images/card-kholmogorskiy.jpg'
+	},
+	{
+		name: 'Байкал',
+		link: 'images/card-baykal.jpg'
+	}
+];
+
+const addCards = (link, name) => {
+	const autoCard = tempCard.cloneNode(true);
+
+	autoCard.querySelector('.card__img').src = link;
+	autoCard.querySelector('.card__description').innerText = name;
+
+	cardsList.append(autoCard);
+}
 
 function popupToggle() {
 	popup.classList.toggle('popup_js-open-close');
@@ -23,6 +61,8 @@ function popupSave(evt) {
 	profileDescription.textContent = popupDescription.value;
 	popup.classList.toggle('popup_js-open-close');
 }
+
+initialCards.forEach(card => addCards(card.link, card.name));
 
 edit.addEventListener('click', popupToggle);
 
