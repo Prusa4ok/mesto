3c70b59994d377df5214d1b3112e106cb565dbd8
Ruslan Mokrov <greenbpost@gmail.com>
2020-09-30 12:04:25 +0600

    hotfix/review1

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ const popupGallery = page.querySelector('.popup_type_gallery');
 const popupFormEdit = page.querySelector('.popup__container_type_edit');
 const popupFormAddCard = page.querySelector('.popup__container_type_addCard');
 const popupButtonAddCard = page.querySelector('.button_type_add');
-const popupButtonClose = page.querySelectorAll('.popup__close');
+const popupButtonsClose = page.querySelectorAll('.popup__close');
 const popupName = page.querySelector('.popup__input_adding_name');
 const popupDescription = page.querySelector('.popup__input_adding_description');
 const popupCardName = page.querySelector('.popup__input_adding_cardName');
@@ -55,8 +55,7 @@ const getButtonsPopups = () => {
 
 // удаление карточки
 const handleDelete = el => {
-	el = el.target.closest('.card');
-	el.remove();
+	el = el.target.closest('.card').remove();
 };
 
 // лайк карточки
@@ -65,74 +64,80 @@ const handleLike = el => {
 };
 
 //разворачиваем картинку для просмотра
-const openImg = (el, img) => {
-	img = el.target.src;
+let determinGalleryItems = (el) => {
 	let currentCard = el.target.closest('.card');
-	let cardDescription = currentCard.querySelector('.card__description').innerText;
-	popupImg = popupGallery.querySelector('.popup__img');
-	popupDesc = popupGallery.querySelector('.popup__desc');
-	popupGallery.classList.toggle('popup_type_open');
-	popupImg.src = img;
-	popupDesc.innerText = cardDescription;
+	let cardDescription = currentCard.querySelector('.card__description').textContent;
+	let popupImg = popupGallery.querySelector('.popup__img');
+	let popupDesc = popupGallery.querySelector('.popup__desc');
+	popupImg.src = el.target.src;
+	popupImg.alt = cardDescription;
+	popupDesc.textContent = cardDescription;
+	openImg();
+}
+
+const openImg = () => {
+	togglePopupOpen(popupGallery);
 }
 
 
 // добавляем карточку, обновляем список кнопок и задаём нажатиям функции
-const addCard = (link, name) => {
+const getCardElement = (link, name) => {
 	const card = tempCard.cloneNode(true);
 
 	card.querySelector('.card__img').src = link;
-	card.querySelector('.card__description').innerText = name;
+	card.querySelector('.card__img').alt = name;
+	card.querySelector('.card__description').textContent = name;
 
-	cardsList.prepend(card);
+	return card;
+}
+
+const addCard = (link, name) => {
+	cardsList.prepend(getCardElement(link, name));
+
 	getButtonsPopups();
 
-	cardsImages.forEach(item => item.addEventListener('click', openImg));
+	cardsImages.forEach(item => item.addEventListener('click', determinGalleryItems));
 	buttonsDelete.forEach(item => item.addEventListener('click', handleDelete));
 	buttonsLike.forEach(item => item.addEventListener('click', handleLike));
 }
 
-function popupEditNameToggle() {
-	popupEditName.classList.toggle('popup_type_open');
-	if (popupEditName.classList.contains('popup_type_open')) {
-		popupName.value = profileName.textContent;
-		popupDescription.value = profileDescription.textContent;
-	}
+function togglePopupEditName() {
+	togglePopupOpen(popupEditName);
+	popupName.value = profileName.textContent;
+	popupDescription.value = profileDescription.textContent;
 }
 
-const popupAddCardCreate = (evt) => {
+const togglePopupOpen = (el) => el.classList.toggle('popup_type_open');
+
+const createPopupAddCard = (evt) => {
 	evt.preventDefault();
 	addCard(popupCardLink.value, popupCardName.value);
 	popupCardLink.value = '';
 	popupCardName.value = '';
-	popupClose();
+	togglePopupOpen(popupAddCard);
 }
 
-function popupEditNameSave(evt) {
+function SavePopupEditName(evt) {
 	evt.preventDefault();
 	profileName.textContent = popupName.value;
 	profileDescription.textContent = popupDescription.value;
-	popupEditName.classList.toggle('popup_type_open');
+	togglePopupOpen(popupEditName);
 }
 
 const popupAddCardToggle = () => {
-	popupAddCard.classList.toggle('popup_type_open');
+	togglePopupOpen(popupAddCard);
 }
 
 initialCards.forEach(card => addCard(card.link, card.name));
 
-edit.addEventListener('click', popupEditNameToggle);
+edit.addEventListener('click', togglePopupEditName);
 popupButtonAddCard.addEventListener('click', popupAddCardToggle);
 
 // закрывание попапа
-const popupClose = () => {
-	popups.forEach(item => {
-		if (item.classList.contains('popup_type_open') != false) {
-			item.classList.toggle('popup_type_open');
-		}
-	})
+const closePopup = (el) => {
+	el.target.closest('.popup').classList.remove('popup_type_open');
 }
 
-popupButtonClose.forEach(item => item.addEventListener('click', popupClose));
-popupFormEdit.addEventListener('submit', popupEditNameSave, false);
-popupFormAddCard.addEventListener('submit', popupAddCardCreate, false);
+popupButtonsClose.forEach(item => item.addEventListener('click', closePopup));
+popupFormEdit.addEventListener('submit', SavePopupEditName);
+popupFormAddCard.addEventListener('submit', createPopupAddCard);
