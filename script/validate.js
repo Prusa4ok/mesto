4624fd7f5e7802c85fee12fcd5877c6fe84830fe
Ruslan Mokrov <greenbpost@gmail.com>
2020-10-05 14:11:42 +0600

    refactoring validate

--- script/validate.js
+++ script/validate.js
@@ -1,9 +1,3 @@
-const inputLength = {
-	popupEditAddName: [2, 40],
-	popupEditAddDesc: [2, 200],
-	popupAddCardName: [2, 30]
-}
-
 const blockButtonAddCard = () => {
 	popupButtonAddCardCreate.disabled = true;
 	if (!popupButtonAddCardCreate.classList.contains('popup__button_type_disabled')) {
@@ -12,20 +6,6 @@ const blockButtonAddCard = () => {
 }
 
 const enableValidation = array => {
-
-	const msgError = (evt, value) => {
-		if (value === 'link-empty') {
-			return 'Вы пропустили это поле.'
-		} else if (value === 'link-error') {
-			return 'Введите адрес сайта.'
-		} else if (value === undefined) {
-			return 'Вы пропустили это поле.'
-		} else if (value === 'less') {
-			return `Минимальное количество символов: ${inputLength[evt.target.id][0]}. Длина текста сейчас: 1 символ.`
-		} else if (value === 'more') {
-			return `Максимальное количество символов: ${inputLength[evt.target.id][1]}. Длина текста сейчас: ${el.target.value.length} символов.`
-		}
-	}
 
 	const checkCardInputs = evt => {
 		const inputs = evt.target.closest(array.formSelector).querySelectorAll(`.${array.inputSelector}`);
@@ -55,19 +35,6 @@ const enableValidation = array => {
 		}
 	}
 
-	const addInputError = (evt, error) => {
-		if (evt.target.classList.contains(array.inputSelector)) {
-			const currentInputId = evt.target.id;
-			const msgErrorEl = page.querySelector(`#${currentInputId}-error`);
-			evt.target.classList.add(array.inputErrorClass);
-			msgErrorEl.classList.remove(array.errorClass);
-			msgErrorEl.textContent = error;
-		}
-		if (!checkCardInputs(evt)) {
-			disabledPopupButton(evt);
-		}
-	}
-
 	const deleteInputError = (evt) => {
 		if (evt.target.classList.contains(array.inputSelector)) {
 			const currentInputId = evt.target.id;
@@ -80,40 +47,16 @@ const enableValidation = array => {
 		}
 	}
 
-	const isUrl = value => {
-		const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-		if (regexp.test(value)) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
 	const checkInputError = evt => {
-		const currentInputLength = evt.target.value.length;
-		let inputLengthText;
-		if (evt.target.id === array.inputLinkSelector) {
-			if (currentInputLength === 0) {
-				addInputError(evt, msgError(evt, 'link-empty'));
-			} else if (!isUrl(evt.target.value)) {
-				addInputError(evt, msgError(evt, 'link-error'));
-			} else if (isUrl(evt.target.value)) {
-				deleteInputError(evt);
-			}
+		const elErrorMessage = evt.target.closest(array.formSelector).querySelector(`#${evt.target.id}-error`);
+		if (!evt.target.reportValidity()) {
+			elErrorMessage.classList.remove(array.errorClass);
+			elErrorMessage.textContent = evt.target.validationMessage;
+			disabledPopupButton(evt);
 		} else {
-			if (currentInputLength === 0) {
-				addInputError(evt, msgError(evt, inputLengthText));
-			} else if (currentInputLength < inputLength[evt.target.id][0]) {
-				inputLengthText = 'less';
-				addInputError(evt, msgError(evt, inputLengthText));
-			} else if (currentInputLength >= inputLength[evt.target.id][0] && currentInputLength <= inputLength[evt.target.id][1]) {
-				inputLengthText = 'matches';
-				deleteInputError(evt, msgError(evt, inputLengthText));
-			} else if (currentInputLength > inputLength[evt.target.id][1]) {
-				inputLengthText = 'more';
-				addInputError(evt, msgError(evt, inputLengthText));
-			}
+			elErrorMessage.classList.remove(array.errorClass);
+			elErrorMessage.textContent = '';
+			deleteInputError(evt)
 		}
 	}
 
