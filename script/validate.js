a56c943cf704bc95221328232b2a634eb2569cca
Ruslan Mokrov <greenbpost@gmail.com>
2020-10-16 16:50:08 +0600

    hotfix refactoring validate.js

--- script/validate.js
+++ script/validate.js
@@ -1,69 +1,73 @@
 const blockButtonAddCard = () => {
 	popupButtonAddCardCreate.disabled = true;
-	if (!popupButtonAddCardCreate.classList.contains('popup__button_type_disabled')) {
-		popupButtonAddCardCreate.classList.add('popup__button_type_disabled');
+	if (!popupButtonAddCardCreate.classList.contains(parameters.inactiveButtonClass)) {
+		popupButtonAddCardCreate.classList.add(parameters.inactiveButtonClass);
 	}
 }
 
-const enableValidation = array => {
+let parameters = {};
 
-	const checkCardInputs = evt => {
-		const inputs = evt.target.closest(array.formSelector).querySelectorAll(`.${array.inputSelector}`);
-		const checking = () => {
-			return !inputs[0].classList.contains(array.inputErrorClass) && !inputs[1].classList.contains(array.inputErrorClass);
-		}
-		if (inputs[0].value.length > 0 && inputs[1].value.length > 0) {
-			return checking();
-		}
-	}
-
-	const enabledPopupButton = evt => {
-		// у каждого свой current container, а при переносе в enableValidation начинает ругаться консоль
-		const currentContainer = evt.target.closest(array.formSelector);
-		const currentButton = currentContainer.querySelector(array.submitButtonSelector);
-		currentContainer.querySelector(array.submitButtonSelector).classList.remove(array.inactiveButtonClass);
-		if (currentButton.disabled === true) {
-			currentButton.disabled = false;
-		}
-	}
-
-	const disabledPopupButton = evt => {
-		const currentContainer = evt.target.closest(array.formSelector);
-		const currentButton = currentContainer.querySelector(array.submitButtonSelector);
-		currentContainer.querySelector(array.submitButtonSelector).classList.add(array.inactiveButtonClass);
-		if (currentButton.disabled === false) {
-			currentButton.disabled = true;
-		}
-	}
-
-	const deleteInputError = (evt) => {
-		if (evt.target.classList.contains(array.inputSelector)) {
-			const currentInputId = evt.target.id;
-			const msgErrorEl = page.querySelector(`#${currentInputId}-error`);
-			evt.target.classList.remove(array.inputErrorClass);
-			msgErrorEl.classList.add(array.errorClass);
-		}
-		if (checkCardInputs(evt)) {
-			enabledPopupButton(evt);
-		}
-	}
-
-	const checkInputError = evt => {
-		const elErrorMessage = evt.target.closest(array.formSelector).querySelector(`#${evt.target.id}-error`);
-		if (!evt.target.reportValidity()) {
-			elErrorMessage.classList.remove(array.errorClass);
-			elErrorMessage.textContent = evt.target.validationMessage;
-			disabledPopupButton(evt);
-		} else {
-			elErrorMessage.classList.remove(array.errorClass);
-			elErrorMessage.textContent = '';
-			deleteInputError(evt)
-		}
-	}
-
-	popupInputs.forEach(item => item.addEventListener('input', checkInputError));
-	popupInputs.forEach(item => item.addEventListener('keyup', checkCardInputs));
+const enableValidation = (object) => {
+	parameters = object;
 }
+
+const checkCardInputs = evt => {
+	const inputs = evt.target.closest(parameters.formSelector).querySelectorAll(`.${parameters.inputSelector}`);
+	const checking = () => {
+		return !inputs[0].classList.contains(parameters.inputErrorClass) && !inputs[1].classList.contains(parameters.inputErrorClass);
+	}
+	if (inputs[0].value.length > 0 && inputs[1].value.length > 0) {
+		return checking();
+	}
+}
+
+
+function enabledPopupButton(evt) {
+	const currentContainer = evt.target.closest(parameters.formSelector);
+	const currentButton = currentContainer.querySelector(parameters.submitButtonSelector);
+	currentContainer.querySelector(parameters.submitButtonSelector).classList.remove(parameters.inactiveButtonClass);
+	if (currentButton.disabled === true) {
+		currentButton.disabled = false;
+	}
+}
+
+const disabledPopupButton = evt => {
+	const currentContainer = evt.target.closest(parameters.formSelector);
+	const currentButton = currentContainer.querySelector(parameters.submitButtonSelector);
+	currentContainer.querySelector(parameters.submitButtonSelector).classList.add(parameters.inactiveButtonClass);
+	if (currentButton.disabled === false) {
+		currentButton.disabled = true;
+	}
+}
+
+const deleteInputError = evt => {
+	if (evt.target.classList.contains(parameters.inputSelector)) {
+		const currentInputId = evt.target.id;
+		const msgErrorEl = page.querySelector(`#${currentInputId}-error`);
+		evt.target.classList.remove(parameters.inputErrorClass);
+		msgErrorEl.classList.add(parameters.errorClass);
+	}
+	if (checkCardInputs(evt)) {
+		enabledPopupButton(evt);
+	}
+}
+
+const checkInputError = evt => {
+	const elErrorMessage = evt.target.closest(parameters.formSelector).querySelector(`#${evt.target.id}-error`);
+	if (!evt.target.reportValidity()) {
+		elErrorMessage.classList.remove(parameters.errorClass);
+		elErrorMessage.textContent = evt.target.validationMessage;
+		disabledPopupButton(evt);
+	} else {
+		elErrorMessage.classList.remove(parameters.errorClass);
+		elErrorMessage.textContent = '';
+		deleteInputError(evt)
+	}
+}
+
+popupInputs.forEach(item => item.addEventListener('input', checkInputError));
+popupInputs.forEach(item => item.addEventListener('keyup', checkCardInputs));
+
 
 enableValidation({
 	formSelector: '.popup__container',
